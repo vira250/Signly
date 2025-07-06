7b397460a5a7932e61ba1db3b60662834b8c7248
vira250 <jviraj666@gmail.com>
2025-07-06 18:01:03 +0530

    enhance

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import joblib
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -40,4 +41,5 @@ def predict():
         return jsonify({"prediction": "Error"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port, debug=True)
